17342fd653c61a969bb4ec6a7e871520a5cd35d6
AurorA1610 <auroralroy2003@gmail.com>
2021-09-03 20:00:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+const API_KEY = `72eb94fa56bdce2a297d2990029e3249`
+const searchTempertaure = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayTemperature(data))
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('weather', temperature.weather[0].main);
+    // Set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
+    console.log(temperature);
+}
